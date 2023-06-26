0a55a1d6d7cd45ed2773bf5d3c7723c51cff7060
Raju Manukonda <dadmom.raju@gmail.com>
2023-06-26 19:06:38 +0530

    Updated app and course.html
    Added fetch request for each subsection to the API and showing the response

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 # app.py
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 # import requests
 # import langchain2.langchain.llms.vertexai
 # import langchain2.langchain.prompts.prompt
+# from langchain import PromptTemplate, LLMChain
 import langchain
+# from google.cloud import aiplatform
 from getpass import getpass
 from langchain.llms import OpenAI
 from langchain import PromptTemplate, LLMChain
 import os
 from jinja2 import Environment
-
-# import google.auth
-# from google.cloud import aiplatform
 
 OPENAI_API_KEY = ''
 
@@ -22,8 +21,13 @@ def jinja2_enumerate(iterable, start=0):
     return enumerate(iterable, start=start)
 
 
+# Create the Flask app
 app = Flask(__name__)
+
+# Register the custom filter
 app.jinja_env.filters['enumerate'] = jinja2_enumerate
+
+toc = {'toc': None}
 
 
 @app.route('/')
@@ -35,8 +39,42 @@ def index():
 def search():
     course_name = request.form['course_name']
     course_contents = generate_course_contents(course_name)
-    toc = {'toc': course_contents}
+    toc['toc'] = course_contents
     return render_template('course.html', **toc)
+
+
+@app.route('/fetch_content', methods=['GET'])
+def fetch_content():
+    content_id = request.args.get('content_id')
+
+    # Extract the chapter index and subsection index from the content_id
+    chapter_index, subsection_index = content_id.split('-')
+
+    # Retrieve the content based on the chapter index and subsection index
+    chapter = toc['toc'][int(chapter_index) - 1]  # Adjust index to 0-based
+    subsection = chapter['subsections'][int(subsection_index)]
+
+    # Use the language model to generate the content
+    generated_content = generate_content(subsection)  # Replace with your language model generation logic
+
+    # Return the generated content as the response
+    return generated_content
+
+
+def generate_content(topic):
+    template = """Question: {question}
+
+    Answer: Let's think step by step."""
+
+    prompt = PromptTemplate(template=template, input_variables=["question"])
+
+    llm = OpenAI()
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+    question = f"Explain me in detail about {topic}. Give one example"
+
+    ans = llm_chain.run(question)
+    return ans
 
 
 def fetch_course_table_of_contents(course_name):
@@ -91,6 +129,7 @@ def generate_course_contents(course_name):
     # Extract and process the relevant information from the API response
     # Generate the course contents with href links
     contents = fetch_course_table_of_contents(course_name)
+
     with open('response.txt', 'w') as f:
         f.write(contents)
 
@@ -102,27 +141,3 @@ if __name__ == '__main__':
     # generate_course_contents("probability")
     app.run()
     # credentials, project_id = google.auth.default()
-    # aiplatform.init(
-    #     # your Google Cloud Project ID or number
-    #     # environment default used is not set
-    #     project='silent-glow-151711',
-    #
-    #     # the Vertex AI region you will use
-    #     # defaults to us-central1
-    #     location='us-central1',
-    #
-    #     # Google Cloud Storage bucket in same region as location
-    #     # used to stage artifacts
-    #     staging_bucket='gs://my_staging_bucket',
-    #
-    #     # custom google.auth.credentials.Credentials
-    #     # environment default credentials used if not set
-    #     credentials=credentials,
-    #
-    #     # the name of the experiment to use to track
-    #     # logged metrics and parameters
-    #     experiment='My-Project',
-    #
-    #     # description of the experiment above
-    #     experiment_description='my experiment description'
-    # )
