f9650fd1f7aedf8a10945ba64542bdc8ac05c911
Jeevan Rao <g1rao@Jeevans-MacBook-Air.local>
2023-07-09 11:31:13 +0530

    Specify external connection

--- app.py
+++ app.py
@@ -190,5 +190,5 @@ def generate_course_contents(course_name):
 
 if __name__ == '__main__':
     # generate_course_contents("probability")
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
     # credentials, project_id = google.auth.default()
